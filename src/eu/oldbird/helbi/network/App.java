fec52964e3e2403d07cbd68529e482e39193b9e9
Gerontopoulos Dimitrios <jim_oldbird1298@hotmail.gr>
2021-01-08 23:37:34 +0200

    Initial Commit 2021!

---
+++ src/eu/oldbird/helbi/network/App.java
@@ -0,0 +1,10 @@
+package eu.oldbird.helbi.network;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Helbi");
+	}
+
+}
